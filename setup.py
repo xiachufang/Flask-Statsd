8ce6a5b88fea2cdb38a363de05edf8ae9c3237d4
Alex.F <gfreezy@gmail.com>
2016-06-08 18:48:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='flask-statsd-tags',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/gfreezy/flask-statsd/',
     license='BSD',
     author='gfreezy',
